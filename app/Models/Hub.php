955676d3d0ac54db050508dc0e4003a3cbc85ea1
MMokhure <201404824@ub.ac.bw>
2021-08-14 01:30:54 +0200

    switch insert

---
+++ app/Models/Hub.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hub extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'model','mserial','gserial','port','data_rate','status','others'
+    ];
+}
